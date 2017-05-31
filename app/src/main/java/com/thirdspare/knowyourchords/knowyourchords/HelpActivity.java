d3fa8b546d2ad07a5123c3018cdfb53c2da9dfcd
MrM11 <michael.ritchie205@sccdsb.net>
2017-05-31 10:54:09 -0400

    added the screens

---
+++ app/src/main/java/com/thirdspare/knowyourchords/knowyourchords/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.thirdspare.knowyourchords.knowyourchords;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by michael.ritchie205 on 5/31/2017.
+ */
+
+public class HelpActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
