b6959174c40289a791dfc127ebc2254bed4d38ab
MrM11 <michael.ritchie205@sccdsb.net>
2017-06-08 10:47:49 -0400

    added tabs to the minor buttons

---
+++ app/src/main/java/com/thirdspare/knowyourchords/knowyourchords/CMinActivity.java
@@ -0,0 +1,16 @@
+package com.thirdspare.knowyourchords.knowyourchords;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by michael.ritchie205 on 6/8/2017.
+ */
+
+public class CMinActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cmin);
+    }
+}
--- app/src/main/java/com/thirdspare/knowyourchords/knowyourchords/MinorActivity.java
+++ app/src/main/java/com/thirdspare/knowyourchords/knowyourchords/MinorActivity.java
@@ -1,7 +1,9 @@
 package com.thirdspare.knowyourchords.knowyourchords;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by michael.ritchie205 on 5/31/2017.
@@ -12,6 +14,51 @@ public class MinorActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minor);
+    }
+
+    public void showAMinorScreen(View view){
+        Intent majorIntent = new Intent(this, AMinActivity.class);
+        startActivity(majorIntent);
+    }
+
+    public void showBMinorScreen(View view){
+        Intent minorIntent = new Intent (this, BMinActivity.class);
+        startActivity(minorIntent);
+    }
+
+    public void showCMinorScreen (View view){
+        Intent helpIntent = new Intent (this, CMinActivity.class);
+        startActivity(helpIntent);
+    }
+
+    public void showDMinorScreen(View view){
+        Intent majorIntent = new Intent(this, DMinActivity.class);
+        startActivity(majorIntent);
+    }
+
+    public void showEMinorScreen(View view){
+        Intent minorIntent = new Intent (this, EMinActivity.class);
+        startActivity(minorIntent);
+    }
+
+    public void showFMinorScreen (View view){
+        Intent helpIntent = new Intent (this, FMinActivity.class);
+        startActivity(helpIntent);
+    }
+
+    public void showGMinorScreen(View view){
+        Intent minorIntent = new Intent (this, GMinActivity.class);
+        startActivity(minorIntent);
+    }
+
+    public void showE7MinorScreen(View view){
+        Intent minorIntent = new Intent (this, E7MinActivity.class);
+        startActivity(minorIntent);
+    }
+
+    public void showA7MinorScreen(View view){
+        Intent minorIntent = new Intent (this, A7MinActivity.class);
+        startActivity(minorIntent);
     }
 }
 
